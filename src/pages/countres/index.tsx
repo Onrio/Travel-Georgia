dc8de568b1f7f24d85ba5bc2003e9181fedcce6a
Anrio <a.silagadze96@gmail.com>
2024-11-11 04:12:51 +0400

    fixes for vercel

--- src/pages/countres/index.tsx
+++ src/pages/countres/index.tsx
@@ -18,108 +18,108 @@ export type Country = {
   georgianAbout: string;
 };
 const CountryList: React.FC<{ lang?: string }> = ({ lang = "en" }) => {
-    const {
-      data,
-      error,
-      isLoading,
-      isFetchingNextPage,
-      fetchNextPage,
-      hasNextPage,
-    } = useInfiniteQuery<Country[], Error>({
-      queryKey: ["allCountries"],
-      queryFn: ({ pageParam = 1 }) =>
-        getAllCountries({ pageParam: pageParam as number }),
-      getNextPageParam: (lastPage, allPages) => {
-        return lastPage.length > 0 ? allPages.length + 1 : undefined;
-      },
-      initialPageParam: 1,
-    });
-  
-    const countries = data?.pages.flat() || [];
-    const parentRef = useRef(null);
-  
-    const rowVirtualizer = useVirtualizer({
-      count: countries.length + (hasNextPage ? 1 : 0),
-      getScrollElement: () => parentRef.current,
-      estimateSize: () => 500,
-      overscan: 5,
-    });
-  
-    rowVirtualizer.getVirtualItems().forEach((virtualRow) => {
-      if (
-        virtualRow.index === countries.length - 1 &&
-        hasNextPage &&
-        !isFetchingNextPage
-      ) {
-        fetchNextPage();
-      }
-    });
-  
-    if (isLoading) return <div>Loading countries...</div>;
-    if (error) return <div>Error: {error.message}</div>;
-  
-    return (
-      <section className={mainStyle["container"]}>
-        <h2 className={mainStyle["section-title"]}>All Countries</h2>
-        <div ref={parentRef} className={style["country-list-container"]}>
-          <div
-            style={{
-              height: rowVirtualizer.getTotalSize(),
-              position: "relative",
-            }}
-          >
-            {rowVirtualizer.getVirtualItems().map((virtualRow: VirtualItem) => {
-              const country = countries[virtualRow.index];
-              if (!country) {
-                return (
-                  <div
-                    key={virtualRow.index}
-                    style={{
-                      height: 500,
-                      transform: `translateY(${virtualRow.start}px)`,
-                    }}
-                  >
-                    {isFetchingNextPage
-                      ? "Loading more countries..."
-                      : "No more countries"}
-                  </div>
-                );
-              }
+  const {
+    data,
+    error,
+    isLoading,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery<Country[], Error>({
+    queryKey: ["allCountries"],
+    queryFn: ({ pageParam = 1 }) =>
+      getAllCountries({ pageParam: pageParam as number }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.length > 0 ? allPages.length + 1 : undefined;
+    },
+    initialPageParam: 1,
+  });
+
+  const countries = data?.pages.flat() || [];
+  const parentRef = useRef(null);
+
+  const rowVirtualizer = useVirtualizer({
+    count: countries.length + (hasNextPage ? 1 : 0),
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 500,
+    overscan: 5,
+  });
+
+  rowVirtualizer.getVirtualItems().forEach((virtualRow) => {
+    if (
+      virtualRow.index === countries.length - 1 &&
+      hasNextPage &&
+      !isFetchingNextPage
+    ) {
+      fetchNextPage();
+    }
+  });
+
+  if (isLoading) return <div>Loading countries...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <section className={mainStyle["container"]}>
+      <h2 className={mainStyle["section-title"]}>All Countries</h2>
+      <div ref={parentRef} className={style["country-list-container"]}>
+        <div
+          style={{
+            height: rowVirtualizer.getTotalSize(),
+            position: "relative",
+          }}
+        >
+          {rowVirtualizer.getVirtualItems().map((virtualRow: VirtualItem) => {
+            const country = countries[virtualRow.index];
+            if (!country) {
               return (
                 <div
-                  key={country.id}
-                  ref={rowVirtualizer.measureElement}
-                  className={style["country-card"]}
-                  data-index={virtualRow.index}
+                  key={virtualRow.index}
                   style={{
+                    height: 500,
                     transform: `translateY(${virtualRow.start}px)`,
                   }}
                 >
-                  <div className={style["country-card-image"]}>
-                    <img
-                      src={country.image}
-                      alt={`${country.name} flag`}
-                      className={style["country-image"]}
-                    />
-                  </div>
-                  <h3 className={style["country-name"]}>
-                    {lang === "ka" ? country.georgianName : country.name}
-                  </h3>
-                  <p>
-                    Capital:{" "}
-                    {lang === "ka" ? country.georgianCapital : country.capital}
-                  </p>
-                  <p>Population: {country.population.toLocaleString()}</p>
-                  <p>{lang === "ka" ? country.georgianAbout : country.about}</p>
-                  <p>Likes: {country.like}</p>
+                  {isFetchingNextPage
+                    ? "Loading more countries..."
+                    : "No more countries"}
                 </div>
               );
-            })}
-          </div>
+            }
+            return (
+              <div
+                key={country.id}
+                ref={rowVirtualizer.measureElement}
+                className={style["country-card"]}
+                data-index={virtualRow.index}
+                style={{
+                  transform: `translateY(${virtualRow.start}px)`,
+                }}
+              >
+                <div className={style["country-card-image"]}>
+                  <img
+                    src={country.image}
+                    alt={`${country.name} flag`}
+                    className={style["country-image"]}
+                  />
+                </div>
+                <h3 className={style["country-name"]}>
+                  {lang === "ka" ? country.georgianName : country.name}
+                </h3>
+                <p>
+                  Capital:{" "}
+                  {lang === "ka" ? country.georgianCapital : country.capital}
+                </p>
+                <p>Population: {country.population.toLocaleString()}</p>
+                <p>{lang === "ka" ? country.georgianAbout : country.about}</p>
+                <p>Likes: {country.like}</p>
+              </div>
+            );
+          })}
         </div>
-        {isFetchingNextPage && <div>Loading more countries...</div>}
-      </section>
-    );
-  };
-  
-  export default CountryList;
+      </div>
+      {isFetchingNextPage && <div>Loading more countries...</div>}
+    </section>
+  );
+};
+
+export default CountryList;
